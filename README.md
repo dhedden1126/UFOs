77259565f38d66e82e675671d9362219be2d35aa
dhedden1126 <dirtdog1126@hotmail.com>
2020-09-21 19:35:47 -0400

    UFO Table

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # UFOs
-UFO Sightings
+
+UFO sighting data creating and deploying HTML and Javascript, while leveraging Bootstrap & CSS for aesthetics. 
+There is 5 interactive filters or search bars for end users to view data they are interested in.
+
+Data includes time, location, and brief descriptions of the events that took place. 
+
---
+++ static/js/app.js
@@ -0,0 +1,84 @@
+// import the data from data.js
+
+const tableData = data;
+
+// reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+    // First, clear out any existing data
+    tbody.html("");
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+      // Append a row to the table body
+      let row = tbody.append("tr");
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        }
+      );
+    });
+  }
+
+  var filters = {};
+
+  function updateFilters() {
+
+  let changesElement = d3.select(this);
+  let elementValue = changesElement.property("value");
+  let filterId = changesElement.attr("id");
+  
+  if (elementValue) {
+    filters[filterId] = elementValue;
+    }
+  else {
+  delete filters[filterId];  
+  };
+  filterTable();
+  }
+
+  function filterTable() {
+  let filteredData = tableData;
+  Object.entries(filters).forEach(([key,value]) =>{
+    filteredData = filteredData.filter(row => row[key] === value);
+  });
+
+  buildTable(filteredData);
+  }
+
+  d3.selectAll("input").on("change", updateFilters);
+  buildTable(tableData);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
